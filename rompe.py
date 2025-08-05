98afc17b71e4f401b8a079957bededa6e5c0245a
jefra <jefra@192.168.1.7>
2025-08-05 14:30:36 -0500

    Versión funcional del juego con mediapipe y pygame

--- rompe.py
+++ rompe.py
@@ -3,3 +3,29 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+pygame.init()
+
+WIDTH, HEIGTH = 800, 600
+screen = pygame.display.set_mode((WIDTH,HEIGTH))
+pygame.display.set_caption("Juego de Romper Bloques")
+running = True
+
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(min_detection_confidence = 0.5,
+                    min_tracking_confidence = 0.5,
+                    max_num_hands = 1) as hands:
+    while running:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imshow('Titulo CV2', frame)
+
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+cap.release()
+cv2.destroyAllWindows()
